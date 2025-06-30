02d313569a6a2f0152b62cb9dba37f128d8686b5
Vkraminho <touafekakram64@gmail.com>
2025-06-30 11:49:36 +0200

    js main

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-.
+const paragraphes = document.querySelectorAll("p");
+
+// Boucle sur chaque paragraphe et ajoute un écouteur d'événement
+paragraphes.forEach(paragraphe => {
+  paragraphe.addEventListener("click", function() {
+    alert("Vous avez cliqué sur : " + this.textContent);
+  });
+});
